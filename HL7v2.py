f18476030e451fe7efa3bfd8582cb75e29115128
DrBenjamin <ben@benbox.org>
2024-03-20 10:33:22 +0100

    added testing

--- HL7v2.py
+++ HL7v2.py
@@ -1,23 +1,29 @@
 import streamlit as st
+import hl7apy
 from hl7apy.parser import parse_message
 from hl7apy.core import Group, Segment
 
+# Function to parse HL7 message
 def parse_hl7_message(message):
     return parse_message(message)
 
-with open('sample_message.txt', 'r') as file:
+# Opening sample document
+with open('sample_message.hl7', 'r') as file:
     message = file.read()
 msg = parse_hl7_message(message)
 
+# Testing
+assert isinstance(parse_hl7_message("MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5"), hl7apy.core.Message)
+
+# Printing the message
 for segment in msg.children:
     if isinstance(segment, Segment):
         for attribute in segment.children:
-            print(attribute, attribute.value)
+            #print(attribute, attribute.value)
             st.write(attribute.value)
     if isinstance(segment, Group):
         for group in segment.children:
             for group_segment in group.children:
                 for attribute in group_segment.children:
-                    print(attribute, attribute.value)
+                    #print(attribute, attribute.value)
                     st.write(attribute.value)
- 
