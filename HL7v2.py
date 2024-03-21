29ad6fcb259d23b5cb63e1849d76913134d7f2ce
DrBenjamin <ben@benbox.org>
2024-03-21 11:24:42 +0100

    added more tests

--- HL7v2.py
+++ HL7v2.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import hl7apy
 from hl7apy.parser import parse_message
 from hl7apy.core import Group, Segment
 
@@ -12,20 +11,17 @@ with open('sample_message.hl7', 'r') as file:
     message = file.read()
 msg = parse_hl7_message(message)
 
-# Testing
-assert isinstance(parse_hl7_message("MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5"), hl7apy.core.Message)
-
 # Printing the message
 st.header("HL7 Message parser")
 st.subheader("HL7 Message (cleaned):")
 for segment in msg.children:
     if isinstance(segment, Segment):
         for attribute in segment.children:
-            #print(attribute, attribute.value)
+            print(attribute, attribute.value)
             st.write(attribute.value)
     if isinstance(segment, Group):
         for group in segment.children:
             for group_segment in group.children:
                 for attribute in group_segment.children:
-                    #print(attribute, attribute.value)
+                    print(attribute, attribute.value)
                     st.write(attribute.value)
--- tests.py
+++ tests.py
@@ -1,14 +1,16 @@
 # tests.py
-import random
 import unittest
+import hl7apy
+from HL7v2 import parse_hl7_message
 
 class SimpleTest(unittest.TestCase):
-    @unittest.skip("demonstrating skipping")
-    def test_skipped(self):
-        self.fail("shouldn't happen")
+    def test_parse_hl7_message(self):
+        self.assertTrue(isinstance(parse_hl7_message(r"""MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5"""), hl7apy.core.Message))
 
-    def test_pass(self):
-        self.assertEqual(10, 7 + 3)
-
-    def test_fail(self):
-        self.assertEqual(11, 7 + 3)
+    def test_parse_hl7_message2(self):
+        self.assertEqual(parse_hl7_message(r"""MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5""").children[0].children[2].value, "HL7Soup")
+        
+    def test_parse_hl7_message3(self):
+        self.assertEqual(parse_hl7_message(r"""MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5""").children[0].children[11].value, "2.5")
+        
+    
