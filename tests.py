af26be17a541deb6039e64847b4de769812c9d9b
DrBenjamin <ben@benbox.org>
2024-03-21 11:35:23 +0100

    minor improvements

--- tests.py
+++ tests.py
@@ -7,10 +7,10 @@ class SimpleTest(unittest.TestCase):
     def test_parse_hl7_message(self):
         self.assertTrue(isinstance(parse_hl7_message(r"""MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5"""), hl7apy.core.Message))
 
-    def test_parse_hl7_message2(self):
+    def test2_parse_hl7_message(self):
         self.assertEqual(parse_hl7_message(r"""MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5""").children[0].children[2].value, "HL7Soup")
         
-    def test_parse_hl7_message3(self):
+    def test3_parse_hl7_message(self):
         self.assertEqual(parse_hl7_message(r"""MSH|^~\&|HL7Soup|Instance1|HL7Soup|Instance2|200808181126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5""").children[0].children[11].value, "2.5")
         
     
