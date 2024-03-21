8fc6800b1e359224f7455ceacb4bf8431c486370
DrBenjamin <ben@benbox.org>
2024-03-21 11:32:38 +0100

    created test.sh script

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 killall python
-/Users/benjamin.gross1/miniconda3/bin/python -m pytest --junitxml results.xml tests.py &
 /Users/benjamin.gross1/miniconda3/bin/python -m streamlit run HL7v2.py &
---
+++ jenkins_test.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+/Users/benjamin.gross1/miniconda3/bin/python -m pytest --junitxml results.xml tests.py &
