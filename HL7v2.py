731932dae7678de56672f8d90856fb3a3e1b9249
DrBenjamin <ben@benbox.org>
2024-06-06 15:28:30 +0200

    added file uploader

--- HL7v2.py
+++ HL7v2.py
@@ -7,28 +7,25 @@ def parse_hl7_message(message):
     return parse_message(message)
 
 # Opening sample document
-with open('sample_message_2.hl7', 'r') as file:
-    message = file.read()
-msg = parse_hl7_message(message)
-
-# Printing the message
-st.header("HL7 Message parser")
-st.subheader("HL7 Message (cleaned):")
-for segment in msg.children:
-    if isinstance(segment, Segment):
-        for attribute in segment.children:
-            print(attribute, attribute.value)
-            st.write(attribute.value)
-    if isinstance(segment, Group):
-        for group in segment.children:
-            for group_segment in group.children:
-                for attribute in group_segment.children:
-                    print(attribute, attribute.value)
-                    st.write(attribute.value)
-
-#TODO: Add file uploader to upload HL7 messages
 file = st.file_uploader("Upload HL7 message", type=['hl7', 'txt'])
 try:
-  st.write(file.getvalue())
+  message = file.read().decode('utf-8')
+  msg = parse_hl7_message(message)
+  
+  # Printing the message
+  st.header("HL7 Message parser")
+  st.subheader("HL7 Message (cleaned):")
+  for segment in msg.children:
+      if isinstance(segment, Segment):
+          for attribute in segment.children:
+              print(attribute, attribute.value)
+              st.write(attribute.value)
+      if isinstance(segment, Group):
+          for group in segment.children:
+              for group_segment in group.children:
+                  for attribute in group_segment.children:
+                      print(attribute, attribute.value)
+                      st.write(attribute.value)
 except Exception as e:
   print(e)
+  #st.write(e)
