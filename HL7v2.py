998466fce1b0fb6775a763fb3c1b85a9349ce5de
DrBenjamin <ben@benbox.org>
2024-06-13 09:13:51 +0200

    minor changes

--- HL7v2.py
+++ HL7v2.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import json
+import re
+from collections import namedtuple
 from hl7apy.parser import parse_message
 from hl7apy.core import Group, Segment
 
@@ -6,15 +10,30 @@ from hl7apy.core import Group, Segment
 def parse_hl7_message(message):
     return parse_message(message)
 
+# Funktion xy
+def parse_message_func(hl7_msg):
+    segments = re.findall(r'(\^[A-Z0-9]{3}\^\d{1,2})([^*]*)', hl7_msg)
+    return [Segment(*segment) for segment in segments]
+
+# Function to convert HL7v2 message to JSON format 
+def convert_to_json(hl7_msg):
+    msg = parse_message_func(hl7_msg)
+    df = pd.DataFrame([[segment.name, segment.fields] for segment in msg])
+    return df
+
 # Opening sample document
 file = st.file_uploader("Upload HL7 message", type=['hl7', 'txt'])
 try:
   message = file.read().decode('utf-8')
-  msg = parse_hl7_message(message)
   
   # Printing the message
   st.header("HL7 Message parser")
   st.subheader("HL7 Message (cleaned):")
+  st.write(convert_to_json(message))
+  
+  # Parsing the message
+  st.subheader("HL7 Message (parsed):")
+  msg = parse_hl7_message(message)
   for segment in msg.children:
       if isinstance(segment, Segment):
           for attribute in segment.children:
@@ -28,4 +47,4 @@ try:
                       st.write(attribute.value)
 except Exception as e:
   print(e)
-  #st.write(e)
+  st.write(e)
