0c07ae295883ddee1668e837a0dfdd4137225623
DrBenjamin <ben@benbox.org>
2024-06-13 10:55:47 +0200

    fixed bug that prevented whole message to be processed

--- HL7v2.py
+++ HL7v2.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import json
-import re
-from collections import namedtuple
 from hl7apy.parser import parse_message
 from hl7apy.core import Group, Segment
 
@@ -10,41 +8,32 @@ from hl7apy.core import Group, Segment
 def parse_hl7_message(message):
     return parse_message(message)
 
-# Funktion xy
-def parse_message_func(hl7_msg):
-    segments = re.findall(r'(\^[A-Z0-9]{3}\^\d{1,2})([^*]*)', hl7_msg)
-    return [Segment(*segment) for segment in segments]
-
-# Function to convert HL7v2 message to JSON format 
-def convert_to_json(hl7_msg):
-    msg = parse_message_func(hl7_msg)
-    df = pd.DataFrame([[segment.name, segment.fields] for segment in msg])
-    return df
-
 # Opening sample document
 file = st.file_uploader("Upload HL7 message", type=['hl7', 'txt'])
 try:
-  message = file.read().decode('utf-8')
-  
-  # Printing the message
-  st.header("HL7 Message parser")
-  st.subheader("HL7 Message (cleaned):")
-  st.write(convert_to_json(message))
+  message = file.read().decode('utf-8').replace('\n', '\r')
   
   # Parsing the message
   st.subheader("HL7 Message (parsed):")
   msg = parse_hl7_message(message)
+  msg_json = []
   for segment in msg.children:
       if isinstance(segment, Segment):
           for attribute in segment.children:
-              print(attribute, attribute.value)
-              st.write(attribute.value)
+              print(attribute.name, attribute.value)
+              st.write(f"{attribute.name}: {attribute.value}")
+              msg_json.append({attribute.name: attribute.value})
       if isinstance(segment, Group):
           for group in segment.children:
               for group_segment in group.children:
                   for attribute in group_segment.children:
-                      print(attribute, attribute.value)
-                      st.write(attribute.value)
+                      print(attribute.name, attribute.value)
+                      st.write(f"{attribute.name}: {attribute.value}")
+                      msg_json.append({attribute.name: attribute.value})
+                      
 except Exception as e:
   print(e)
-  st.write(e)
+
+# Saving to JSON file
+with open('output.json', 'w') as json_file:
+    json.dump(msg_json, json_file)
